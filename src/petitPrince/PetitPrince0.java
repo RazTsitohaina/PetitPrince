9e4fc2b86427634b55bffeba4286d736dc12ebbb
Crack <Crack@172.17.158.127>
2022-11-24 15:00:59 +0100

    PetitPrince

---
+++ src/petitPrince/PetitPrince0.java
@@ -0,0 +1,27 @@
+package petitPrince;
+
+import asteroide.Sujet;
+
+public class PetitPrince0 implements Sujet{
+	
+	private String nom;
+	private int argent;
+	
+	public PetitPrince0(String nom, int argent) {
+		this.nom = nom;
+		this.argent = argent;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+    
+	public int getArgent() {
+		return this.argent;
+	}
+    
+	public void run() {
+		
+	}
+
+}
